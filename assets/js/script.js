541d6cbe3c1752ed20c185e236dd3c03c658c67c
Krishan Gharu <kslg257@gmail.com>
2022-03-30 10:56:43 +0000

    html updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
 //Question bank
 var questionBank= [
     {
-        question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
-        option : ['Asia','Africa','Europe','Australia'],
-        answer : 'Africa'
+        question : 'Question 1',
+        option : ['Answer1','Answer2','Answer3','Answer4'],
+        answer : 'Answer1'
     },
     {
-        question : 'Garampani sanctuary is located at',
-        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
-        answer : 'Diphu, Assam'
+        question : 'Question 2',
+        option : ['Answer1','Answer2','Answer3','Answer4'],
+        answer : 'Answer1'
     },
     {
-        question : 'For which of the following disciplines is Nobel Prize awarded?',
-        option : ['Physics and Chemistry','Physiology or Medicine','Literature, Peace and Economics','All of the above'],
-        answer : 'All of the above'
+        question : 'Question 3',
+        option : ['Answer1','Answer2','Answer3','Answer4'],
+        answer : 'Answer1'
     },
     {
-        question : 'Hitler party which came into power in 1933 is known as',
-        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
-        answer : 'Nazi Party'
+        question : 'Question 4',
+        option : ['Answer1','Answer2','Answer3','Answer4'],
+        answer : 'Answer1'
     },
     {
-        question : 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
-        option : ['1967','1968','1958','1922'],
-        answer : '1967'
+        question : 'Question 5',
+        option : ['Answer1','Answer2','Answer3','Answer4'],
+        answer : 'Answer1'
     }
 ]
 
@@ -114,9 +114,11 @@ const countdownEl = document.getElementById('countdown');
 setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
-    const minutes = Math.floor(time / 60):
-    let seconds = time % 60:
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
 
     countdownEl.innerHTML = `${minutes}: ${seconds}`;
-    time--)
+    time--;
 }
