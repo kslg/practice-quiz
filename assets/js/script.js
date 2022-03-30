119272933492b053a7a8a9fec86d5af4974c2c32
Krishan Gharu <kslg257@gmail.com>
2022-03-30 21:50:49 +0000

    updated countdown timer and cleaned up js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,48 @@
 //Question bank
-var questionBank= [
+var questionBank = [
     {
         question : 'Question 1',
-        option : ['Answer1','Answer2','Answer3','Answer4'],
+        option : ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
         answer : 'Answer1'
     },
     {
         question : 'Question 2',
-        option : ['Answer1','Answer2','Answer3','Answer4'],
+        option : ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
         answer : 'Answer1'
     },
     {
         question : 'Question 3',
-        option : ['Answer1','Answer2','Answer3','Answer4'],
+        option : ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
         answer : 'Answer1'
     },
     {
         question : 'Question 4',
-        option : ['Answer1','Answer2','Answer3','Answer4'],
+        option : ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
         answer : 'Answer1'
     },
     {
         question : 'Question 5',
-        option : ['Answer1','Answer2','Answer3','Answer4'],
+        option : ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
         answer : 'Answer1'
     }
-]
+];
 
-var question= document.getElementById('question');
-var quizContainer= document.getElementById('quiz-container');
-var scorecard= document.getElementById('scorecard');
-var option0= document.getElementById('option0');
-var option1= document.getElementById('option1');
-var option2= document.getElementById('option2');
-var option3= document.getElementById('option3');
-var next= document.querySelector('.next');
-var points= document.getElementById('score');
-var span= document.querySelectorAll('span');
-var i=0;
-var score= 0;
+var question = document.getElementById('question');
+var quizContainer = document.getElementById('quiz-container');
+var scorecard = document.getElementById('scorecard');
+var option0 = document.getElementById('option0');
+var option1 = document.getElementById('option1');
+var option2 = document.getElementById('option2');
+var option3 = document.getElementById('option3');
+var next = document.querySelector('.next');
+var points = document.getElementById('score');
+var span = document.querySelectorAll('span');
+var i = 0;
+var score = 0;
 
 //Function to display questions
-function displayQuestion(){
-    for(var a=0;a<span.length;a++){
+function displayQuestion() {
+    for (var a=0;a<span.length;a++){
         span[a].style.background='none';
     }
     question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
@@ -55,12 +55,10 @@ function displayQuestion(){
 
 //Function to calculate scores
 function calcScore(e){
-    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
-    {
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length){
         score= score+1;
         document.getElementById(e.id).style.background= 'limegreen';
-    }
-    else{
+    } else {
         document.getElementById(e.id).style.background= 'tomato';
     }
     setTimeout(nextQuestion,300);
@@ -72,8 +70,7 @@ function nextQuestion(){
     {
         i=i+1;
         displayQuestion();
-    }
-    else{
+    } else {
         points.innerHTML= score+ '/'+ questionBank.length;
         quizContainer.style.display= 'none';
         scoreboard.style.display= 'block'
@@ -91,11 +88,12 @@ function backToQuiz(){
 //Function to check Answers
 function checkAnswer(){
     var answerBank= document.getElementById('answerBank');
+    
     var answers= document.getElementById('answers');
     answerBank.style.display= 'block';
     scoreboard.style.display= 'none';
-    for(var a=0;a<questionBank.length;a++)
-    {
+    
+    for(var a=0;a<questionBank.length;a++) {
         var list= document.createElement('li');
         list.innerHTML= questionBank[a].answer;
         answers.appendChild(list);
@@ -105,31 +103,17 @@ function checkAnswer(){
 
 displayQuestion();
 
+// Countdown timer
 
-//Countdown timer
-const startingMinutes  = 1;
-let time = startingMinutes * 60;
-
-const countdownEl = document.getElementById('countdown');
-
-// Call the function below every second (1000 miliseconds)
-setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-
-// using ternary operator so the timer counts down properly
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-
-    countdownEl.innerHTML = `${minutes}: ${seconds}`;
-    time--;
-    
-    if (seconds === 55) {
-        clearTimeout(updateCountdown); 
-        alert(`Oh no! You've have run out of time. Don't worry, you can try again.`); // displays prompt message on screen to the user
-        throw `Player ran out of time. Need to restart the game`; // log an error in the console
-        
-    } 
-   
-}
+var timeleft = 60;
+var downloadTimer = setInterval(function(){
+    if(timeleft === -1){
+        clearInterval(downloadTimer);
+        document.getElementById("countdown").innerHTML = "Times Up!";
+        alert(`Oh no! You've have run out of time. Hit the reset button and try again.`); // displays prompt message on screen to the user
+        throw `Player ran out of time. Need to restart the game`; // logs an error in the console
+    } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+    }
+    timeleft -= 1;
+}, 1000);
